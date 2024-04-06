db886e4d97a422894344a886f5aa52bef4f9d4cb
nakanoyuki <yukich41@gmail.com>
2024-04-06 17:40:51 +0900

    ✨: logout component追加

---
+++ src/components/Logout/Logout.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@chakra-ui/react";
+import { useAuthContext } from "../../hooks/AuthProvider";
+import { useRouter } from "next/router";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../lib/firebaseConfig";
+
+const Logout = () => {
+  const router = useRouter();
+  const { user } = useAuthContext();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    router.push("/login");
+  };
+  return (
+    <>
+      <Button
+        sx={{
+          color: "#4169e1",
+          fontSize: "14px",
+          fontWeight: "600",
+          ml: "4px",
+        }}
+        onClick={handleLogout}
+      >
+        ログアウト
+      </Button>
+    </>
+  );
+};
+
+export default Logout;

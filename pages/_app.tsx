b337db5a0b83d28cd5b11f60d80376a69ee58327
nakanoyuki <yukich41@gmail.com>
2024-04-06 17:23:36 +0900

    ✨: AuthProvierをapp.tsxに設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,19 @@ import { ChakraBaseProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { extendTheme } from "@chakra-ui/react";
 import Layout from "../src/components/Layout/Layout";
+import { AuthProvider } from "../src/hooks/AuthProvider";
 
 const theme = extendTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraBaseProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraBaseProvider>
+    <AuthProvider>
+      <ChakraBaseProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraBaseProvider>
+    </AuthProvider>
   );
 }
 

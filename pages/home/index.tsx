ab4d204cccd485ac9e4fd2431a52d6e1250e2809
nakanoyuki <yukich41@gmail.com>
2024-04-06 17:23:58 +0900

    ✨: Home componentの追加

---
+++ pages/home/index.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+
+function Home() {
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      width="1200px"
+      maxW="100%"
+      m="auto"
+    >
+      Homeのフォーム
+    </Flex>
+  );
+}
+
+export default Home;

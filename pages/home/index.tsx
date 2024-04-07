8cc2f8f3c59825371fb2a2a2b6b900d7345e2bd0
nakanoyuki <yukich41@gmail.com>
2024-04-07 23:47:19 +0900

    ✨: home conponentsにuserの状態での出し分けを設定

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,17 +1,28 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import React from "react";
+import { useAuthContext } from "../../src/hooks/AuthProvider";
+import Login from "../login";
 
 function Home() {
+  const { user } = useAuthContext();
   return (
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      width="1200px"
-      maxW="100%"
-      m="auto"
-    >
-      Homeのフォーム
-    </Flex>
+    <>
+      {user ? (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="1200px"
+          maxW="100%"
+          m="auto"
+        >
+          Homeのフォーム
+        </Flex>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
+    </>
   );
 }
 

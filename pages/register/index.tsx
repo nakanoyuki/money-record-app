de206e8f5facd3e7e1a7e0b08f3cf07245f882b8
nakanoyuki <yukich41@gmail.com>
2024-04-10 00:29:17 +0900

    ✨: register pageにuserでの出し分け追加

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -11,10 +11,14 @@ import {
   Input,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { useAuthContext } from "../../src/hooks/AuthProvider";
+import Home from "../home";
 
 export default function Register() {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
+  const { user } = useAuthContext();
+
   const router = useRouter();
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -32,62 +36,70 @@ export default function Register() {
   };
 
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex
-        direction="column"
-        background="gray.100"
-        padding={8}
-        rounded={6}
-        w="350px"
-        backgroundColor="white"
-      >
-        <Heading as="h1" fontSize="lg" textAlign="center">
-          新規登録
-        </Heading>
-        <form onSubmit={handleSubmit}>
-          <FormControl isRequired mt={4}>
-            <FormLabel fontSize="sm">メールアドレス</FormLabel>
-            <Input
-              placeholder="sample@sample.com"
-              type="email"
-              background="white"
-              focusBorderColor="gray.100"
-              fontSize="sm"
-              errorBorderColor="red.300"
-              padding={2}
-              onChange={(e) => setRegisterEmail(e.target.value)}
-            />
-            <FormErrorMessage>
-              メールアドレスを入力してください
-            </FormErrorMessage>
-          </FormControl>
-
-          <FormControl isRequired mt={4}>
-            <FormLabel fontSize="sm">パスワード</FormLabel>
-            <Input
-              placeholder="********"
-              focusBorderColor="gray.100"
-              type="password"
-              background="white"
-              fontSize="sm"
-              errorBorderColor="red.300"
-              padding={2}
-              onChange={(e) => setRegisterPassword(e.target.value)}
-            />
-            <FormErrorMessage>パスワードを入力してください</FormErrorMessage>
-          </FormControl>
-          <Button
-            fontSize="lg"
-            mt={4}
-            w="100%"
-            backgroundColor="#4169e1"
-            color="white"
-            type="submit"
+    <>
+      {user ? (
+        <Home />
+      ) : (
+        <Flex height="100vh" alignItems="center" justifyContent="center">
+          <Flex
+            direction="column"
+            background="gray.100"
+            padding={8}
+            rounded={6}
+            w="350px"
+            backgroundColor="white"
           >
-            登録
-          </Button>
-        </form>
-      </Flex>
-    </Flex>
+            <Heading as="h1" fontSize="lg" textAlign="center">
+              新規登録
+            </Heading>
+            <form onSubmit={handleSubmit}>
+              <FormControl isRequired mt={4}>
+                <FormLabel fontSize="sm">メールアドレス</FormLabel>
+                <Input
+                  placeholder="sample@sample.com"
+                  type="email"
+                  background="white"
+                  focusBorderColor="gray.100"
+                  fontSize="sm"
+                  errorBorderColor="red.300"
+                  padding={2}
+                  onChange={(e) => setRegisterEmail(e.target.value)}
+                />
+                <FormErrorMessage>
+                  メールアドレスを入力してください
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl isRequired mt={4}>
+                <FormLabel fontSize="sm">パスワード</FormLabel>
+                <Input
+                  placeholder="********"
+                  focusBorderColor="gray.100"
+                  type="password"
+                  background="white"
+                  fontSize="sm"
+                  errorBorderColor="red.300"
+                  padding={2}
+                  onChange={(e) => setRegisterPassword(e.target.value)}
+                />
+                <FormErrorMessage>
+                  パスワードを入力してください
+                </FormErrorMessage>
+              </FormControl>
+              <Button
+                fontSize="lg"
+                mt={4}
+                w="100%"
+                backgroundColor="#4169e1"
+                color="white"
+                type="submit"
+              >
+                登録
+              </Button>
+            </form>
+          </Flex>
+        </Flex>
+      )}
+    </>
   );
 }

433f4df1d21c6a4eb0883874a7b303893f847b9c
nakanoyuki <yukich41@gmail.com>
2024-04-10 00:16:08 +0900

    ✨: ログインしている場合は/homeにリダイレクト

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 import { FirstView } from "../src/components/Firstview";
+import { useAuthContext } from "../src/hooks/AuthProvider";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const { user } = useAuthContext();
+  const router = useRouter();
+  useEffect(() => {
+    if (user) {
+      router.push("/home");
+    }
+  }, [user, router]);
   return <FirstView />;
 };
 

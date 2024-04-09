fac6abc338d710c593630ef411c9c52dbcdba646
nakanoyuki <yukich41@gmail.com>
2024-04-09 23:55:38 +0900

    🗂: InputRecordForm componentを新規追加

---
+++ src/components/InputRecordForm/InputRecordForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import InputRecordFormHeading from "./InputRecordFormHeading";
+import InputRecordFormTable from "./InputRecordFormTable";
+
+const InputRecordForm = () => {
+  return (
+    <>
+      <InputRecordFormHeading />
+      <InputRecordFormTable />
+    </>
+  );
+};
+
+export default InputRecordForm;

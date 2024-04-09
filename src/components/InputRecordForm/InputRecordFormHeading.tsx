b950b4c6f975dbf159020b891f475f38bc5d3ace
nakanoyuki <yukich41@gmail.com>
2024-04-09 23:57:05 +0900

    InputRecordFormHeading componentsを追加・月を取得する関数追加

---
+++ src/components/InputRecordForm/InputRecordFormHeading.tsx
@@ -0,0 +1,22 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+const InputRecordFormHeading = () => {
+  const currentDate = new Date();
+  const getCurrentMonth = currentDate.getMonth() + 1;
+  return (
+    <>
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        width="1200px"
+        maxW="100%"
+        m="auto"
+      >
+        <Text fontSize="md">今月{getCurrentMonth}月の収支</Text>
+      </Flex>
+    </>
+  );
+};
+
+export default InputRecordFormHeading;

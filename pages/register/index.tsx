d1b398c072fb34dffefe60d14bc92c02e707d5e6
nakanoyuki <yukich41@gmail.com>
2024-04-15 23:02:30 +0900

    ♻️: RegisterBlock componentに分離

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,105 +1,10 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
-import { auth } from "../../lib/firebaseConfig";
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-} from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import React from "react";
 import { useAuthContext } from "../../src/hooks/AuthProvider";
 import Home from "../home";
+import RegisterBlock from "../../src/components/RegisterBlock/RegisterBlock";
 
 export default function Register() {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
   const { user } = useAuthContext();
 
-  const router = useRouter();
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
-    try {
-      await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-      );
-      router.push("/home");
-    } catch (error) {
-      alert("正しく入力してください");
-    }
-  };
-
-  return (
-    <>
-      {user ? (
-        <Home />
-      ) : (
-        <Flex height="100vh" alignItems="center" justifyContent="center">
-          <Flex
-            direction="column"
-            background="gray.100"
-            padding={8}
-            rounded={6}
-            w="350px"
-            backgroundColor="white"
-          >
-            <Heading as="h1" fontSize="lg" textAlign="center">
-              新規登録
-            </Heading>
-            <form onSubmit={handleSubmit}>
-              <FormControl isRequired mt={4}>
-                <FormLabel fontSize="sm">メールアドレス</FormLabel>
-                <Input
-                  placeholder="sample@sample.com"
-                  type="email"
-                  background="white"
-                  focusBorderColor="gray.100"
-                  fontSize="sm"
-                  errorBorderColor="red.300"
-                  padding={2}
-                  onChange={(e) => setRegisterEmail(e.target.value)}
-                />
-                <FormErrorMessage>
-                  メールアドレスを入力してください
-                </FormErrorMessage>
-              </FormControl>
-
-              <FormControl isRequired mt={4}>
-                <FormLabel fontSize="sm">パスワード</FormLabel>
-                <Input
-                  placeholder="********"
-                  focusBorderColor="gray.100"
-                  type="password"
-                  background="white"
-                  fontSize="sm"
-                  errorBorderColor="red.300"
-                  padding={2}
-                  onChange={(e) => setRegisterPassword(e.target.value)}
-                />
-                <FormErrorMessage>
-                  パスワードを入力してください
-                </FormErrorMessage>
-              </FormControl>
-              <Button
-                fontSize="lg"
-                mt={4}
-                w="100%"
-                backgroundColor="#4169e1"
-                color="white"
-                type="submit"
-              >
-                登録
-              </Button>
-            </form>
-          </Flex>
-        </Flex>
-      )}
-    </>
-  );
+  return <>{user ? <Home /> : <RegisterBlock />}</>;
 }
---
+++ src/components/RegisterBlock/RegisterBlock.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../lib/firebaseConfig";
+import { useRouter } from "next/router";
+
+const RegisterBlock = () => {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      router.push("/home");
+    } catch (error) {
+      alert("正しく入力してください");
+    }
+  };
+
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        direction="column"
+        background="gray.100"
+        padding={8}
+        rounded={6}
+        w="350px"
+        backgroundColor="white"
+      >
+        <Heading as="h1" fontSize="lg" textAlign="center">
+          新規登録
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <FormControl isRequired mt={4}>
+            <FormLabel fontSize="sm">メールアドレス</FormLabel>
+            <Input
+              placeholder="sample@sample.com"
+              type="email"
+              background="white"
+              focusBorderColor="gray.100"
+              fontSize="sm"
+              errorBorderColor="red.300"
+              padding={2}
+              onChange={(e) => setRegisterEmail(e.target.value)}
+            />
+            <FormErrorMessage>
+              メールアドレスを入力してください
+            </FormErrorMessage>
+          </FormControl>
+
+          <FormControl isRequired mt={4}>
+            <FormLabel fontSize="sm">パスワード</FormLabel>
+            <Input
+              placeholder="********"
+              focusBorderColor="gray.100"
+              type="password"
+              background="white"
+              fontSize="sm"
+              errorBorderColor="red.300"
+              padding={2}
+              onChange={(e) => setRegisterPassword(e.target.value)}
+            />
+            <FormErrorMessage>パスワードを入力してください</FormErrorMessage>
+          </FormControl>
+          <Button
+            fontSize="lg"
+            mt={4}
+            w="100%"
+            backgroundColor="#4169e1"
+            color="white"
+            type="submit"
+          >
+            登録
+          </Button>
+        </form>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default RegisterBlock;

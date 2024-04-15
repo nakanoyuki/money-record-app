326b3566f0bc6f56669d54f58ded6cf181afbdd6
nakanoyuki <yukich41@gmail.com>
2024-04-15 22:58:48 +0900

    🗂: add InputForm component file

---
+++ pages/input-form/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InputRecordForm from "../../src/components/InputRecordForm/InputRecordForm";
+
+const InputForm = () => {
+  return <InputRecordForm />;
+};
+
+export default InputForm;

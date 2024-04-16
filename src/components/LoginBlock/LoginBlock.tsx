1a0726661b4f4c1ba4bbe528acc1609443c366f1
nakanoyuki <yukich41@gmail.com>
2024-04-16 09:03:02 +0900

    ✨: 遷移中にローディング追加

--- src/components/LoginBlock/LoginBlock.tsx
+++ src/components/LoginBlock/LoginBlock.tsx
@@ -9,81 +9,102 @@ import {
   FormLabel,
   Heading,
   Input,
+  Spinner,
 } from "@chakra-ui/react";
 import { auth } from "../../../lib/firebaseConfig";
 
 const LoginBlock = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
   const router = useRouter();
 
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     try {
+      setIsLoading(true);
       await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
       router.push("/home");
     } catch (error) {
       alert("メールアドレスまたはパスワードが間違っています");
     }
+    setIsLoading(false);
   };
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex
-        direction="column"
-        background="gray.100"
-        padding={8}
-        rounded={6}
-        w="350px"
-        backgroundColor="white"
-      >
-        <Heading as="h1" fontSize="lg" textAlign="center">
-          ログイン
-        </Heading>
-        <form onSubmit={handleSubmit}>
-          <FormControl isRequired mt={4}>
-            <FormLabel fontSize="sm">メールアドレス</FormLabel>
-            <Input
-              placeholder="sample@sample.com"
-              type="email"
-              background="white"
-              focusBorderColor="gray.100"
-              fontSize="sm"
-              errorBorderColor="red.300"
-              padding={2}
-              onChange={(e) => setLoginEmail(e.target.value)}
-            />
-            <FormErrorMessage>
-              メールアドレスを入力してください
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl isRequired mt={4}>
-            <FormLabel fontSize="sm">パスワード</FormLabel>
-            <Input
-              placeholder="********"
-              focusBorderColor="gray.100"
-              type="password"
-              background="white"
-              fontSize="sm"
-              errorBorderColor="red.300"
-              padding={2}
-              onChange={(e) => setLoginPassword(e.target.value)}
-            />
-            <FormErrorMessage>パスワードを入力してください</FormErrorMessage>
-          </FormControl>
-          <Button
-            fontSize="lg"
-            mt={4}
-            w="100%"
-            backgroundColor="#4169e1"
-            color="white"
-            type="submit"
+    <>
+      {isLoading ? (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="1200px"
+          maxW="100%"
+          m="auto"
+          color="#4169e1"
+        >
+          <Spinner />
+        </Flex>
+      ) : (
+        <Flex height="100vh" alignItems="center" justifyContent="center">
+          <Flex
+            direction="column"
+            background="gray.100"
+            padding={8}
+            rounded={6}
+            w="350px"
+            backgroundColor="white"
           >
-            ログイン
-          </Button>
-        </form>
-      </Flex>
-    </Flex>
+            <Heading as="h1" fontSize="lg" textAlign="center">
+              ログイン
+            </Heading>
+            <form onSubmit={handleSubmit}>
+              <FormControl isRequired mt={4}>
+                <FormLabel fontSize="sm">メールアドレス</FormLabel>
+                <Input
+                  placeholder="sample@sample.com"
+                  type="email"
+                  background="white"
+                  focusBorderColor="gray.100"
+                  fontSize="sm"
+                  errorBorderColor="red.300"
+                  padding={2}
+                  onChange={(e) => setLoginEmail(e.target.value)}
+                />
+                <FormErrorMessage>
+                  メールアドレスを入力してください
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isRequired mt={4}>
+                <FormLabel fontSize="sm">パスワード</FormLabel>
+                <Input
+                  placeholder="********"
+                  focusBorderColor="gray.100"
+                  type="password"
+                  background="white"
+                  fontSize="sm"
+                  errorBorderColor="red.300"
+                  padding={2}
+                  onChange={(e) => setLoginPassword(e.target.value)}
+                />
+                <FormErrorMessage>
+                  パスワードを入力してください
+                </FormErrorMessage>
+              </FormControl>
+              <Button
+                fontSize="lg"
+                mt={4}
+                w="100%"
+                backgroundColor="#4169e1"
+                color="white"
+                type="submit"
+              >
+                ログイン
+              </Button>
+            </form>
+          </Flex>
+        </Flex>
+      )}
+    </>
   );
 };
 
--- src/components/RegisterBlock/RegisterBlock.tsx
+++ src/components/RegisterBlock/RegisterBlock.tsx
@@ -7,20 +7,23 @@ import {
   FormLabel,
   Heading,
   Input,
+  Spinner,
 } from "@chakra-ui/react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../lib/firebaseConfig";
 import { useRouter } from "next/router";
 
 const RegisterBlock = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const router = useRouter();
 
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     try {
+      setIsLoading(true);
       await createUserWithEmailAndPassword(
         auth,
         registerEmail,
@@ -30,66 +33,84 @@ const RegisterBlock = () => {
     } catch (error) {
       alert("正しく入力してください");
     }
+    setIsLoading(false);
   };
 
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex
-        direction="column"
-        background="gray.100"
-        padding={8}
-        rounded={6}
-        w="350px"
-        backgroundColor="white"
-      >
-        <Heading as="h1" fontSize="lg" textAlign="center">
-          新規登録
-        </Heading>
-        <form onSubmit={handleSubmit}>
-          <FormControl isRequired mt={4}>
-            <FormLabel fontSize="sm">メールアドレス</FormLabel>
-            <Input
-              placeholder="sample@sample.com"
-              type="email"
-              background="white"
-              focusBorderColor="gray.100"
-              fontSize="sm"
-              errorBorderColor="red.300"
-              padding={2}
-              onChange={(e) => setRegisterEmail(e.target.value)}
-            />
-            <FormErrorMessage>
-              メールアドレスを入力してください
-            </FormErrorMessage>
-          </FormControl>
-
-          <FormControl isRequired mt={4}>
-            <FormLabel fontSize="sm">パスワード</FormLabel>
-            <Input
-              placeholder="********"
-              focusBorderColor="gray.100"
-              type="password"
-              background="white"
-              fontSize="sm"
-              errorBorderColor="red.300"
-              padding={2}
-              onChange={(e) => setRegisterPassword(e.target.value)}
-            />
-            <FormErrorMessage>パスワードを入力してください</FormErrorMessage>
-          </FormControl>
-          <Button
-            fontSize="lg"
-            mt={4}
-            w="100%"
-            backgroundColor="#4169e1"
-            color="white"
-            type="submit"
+    <>
+      {isLoading ? (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="1200px"
+          maxW="100%"
+          m="auto"
+          color="#4169e1"
+        >
+          <Spinner />
+        </Flex>
+      ) : (
+        <Flex height="100vh" alignItems="center" justifyContent="center">
+          <Flex
+            direction="column"
+            background="gray.100"
+            padding={8}
+            rounded={6}
+            w="350px"
+            backgroundColor="white"
           >
-            登録
-          </Button>
-        </form>
-      </Flex>
-    </Flex>
+            <Heading as="h1" fontSize="lg" textAlign="center">
+              新規登録
+            </Heading>
+            <form onSubmit={handleSubmit}>
+              <FormControl isRequired mt={4}>
+                <FormLabel fontSize="sm">メールアドレス</FormLabel>
+                <Input
+                  placeholder="sample@sample.com"
+                  type="email"
+                  background="white"
+                  focusBorderColor="gray.100"
+                  fontSize="sm"
+                  errorBorderColor="red.300"
+                  padding={2}
+                  onChange={(e) => setRegisterEmail(e.target.value)}
+                />
+                <FormErrorMessage>
+                  メールアドレスを入力してください
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl isRequired mt={4}>
+                <FormLabel fontSize="sm">パスワード</FormLabel>
+                <Input
+                  placeholder="********"
+                  focusBorderColor="gray.100"
+                  type="password"
+                  background="white"
+                  fontSize="sm"
+                  errorBorderColor="red.300"
+                  padding={2}
+                  onChange={(e) => setRegisterPassword(e.target.value)}
+                />
+                <FormErrorMessage>
+                  パスワードを入力してください
+                </FormErrorMessage>
+              </FormControl>
+              <Button
+                fontSize="lg"
+                mt={4}
+                w="100%"
+                backgroundColor="#4169e1"
+                color="white"
+                type="submit"
+              >
+                登録
+              </Button>
+            </form>
+          </Flex>
+        </Flex>
+      )}
+    </>
   );
 };
 

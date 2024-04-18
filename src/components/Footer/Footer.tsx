459ca092d89b909f8dba1788daf9d9067618d223
nakanoyuki <yukich41@gmail.com>
2024-04-19 08:53:59 +0900

    📝: Footerの文言変更

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
+import { Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <Stack as="footer">
+      <Text fontSize="xs" textAlign="center" color="#707070" pt="10px">
+        Powered By 収支管理アプリ
+      </Text>
+    </Stack>
+  );
 }
 
 export default Footer;

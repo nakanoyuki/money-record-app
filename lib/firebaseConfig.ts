901b9e242cf86053ebea2741911902755797fdd2
nakanoyuki <yukich41@gmail.com>
2024-04-22 23:28:22 +0900

    🔥: 不要な値の削除

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -9,8 +9,6 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
   appId: process.env.NEXT_PUBLIC_APPID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
 const app = initializeApp(firebaseConfig);

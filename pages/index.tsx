ff7ef00219953753a47b066d19e085d1abd01b21
nakanoyuki <yukich41@gmail.com>
2024-04-22 23:30:00 +0900

    🐛: 遷移中のチラつきをなくす

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,22 @@ import type { NextPage } from "next";
 import { FirstView } from "../src/components/Firstview";
 import { useAuthContext } from "../src/hooks/AuthProvider";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
   const { user } = useAuthContext();
+  const [loading, setLoading] = useState(true);
+
   const router = useRouter();
+
   useEffect(() => {
     if (user) {
       router.push("/home");
+      return;
     }
+    setLoading(false);
   }, [user, router]);
-  return <FirstView />;
+  return loading ? <div>Loading!!</div> : <FirstView />;
 };
 
 export default Home;

9a183efa62183de7dc91404dce4c6432ae62f844
nakanoyuki <yukich41@gmail.com>
2024-05-05 11:30:00 +0900

    ✨: 月の収支の表示を追加

--- src/components/DisplayRecord/DisplayRecord.tsx
+++ src/components/DisplayRecord/DisplayRecord.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export const DisplayRecord = () => {
-  return <div>DisplayRecord</div>;
+  const currentDate = new Date();
+  const getCurrentMonth = currentDate.getMonth() + 1;
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Text fontSize="md">{getCurrentMonth}月の収支</Text>
+    </Flex>
+  );
 };

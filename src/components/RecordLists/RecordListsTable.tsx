823b468c74d0d549d8ca6db8440434c92b513e6b
nakanoyuki <yukich41@gmail.com>
2024-05-11 21:15:07 +0900

    ✨: 登録したデータを一覧ページで表示

--- src/components/RecordLists/RecordListsTable.tsx
+++ src/components/RecordLists/RecordListsTable.tsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Thead, Tbody, Tr, Th, Td, Box, Flex } from "@chakra-ui/react";
+import {
+  CollectionReference,
+  Timestamp,
+  collection,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "../../../lib/firebaseConfig";
+
+export type FormType = {
+  id: string;
+  date: Timestamp;
+  amount: string;
+  type: string;
+  category: string;
+  paymentType: string;
+  description: string;
+  uid: string | undefined;
+};
 
 const RecordListsTable = () => {
-  const expenses = [
-    {
-      id: 1,
-      date: "2024-05-05",
-      type: "支出",
-      amount: 1000,
-      description: "食料品購入",
-    },
-    {
-      id: 2,
-      date: "2024-05-06",
-      type: "収入",
-      amount: 5000,
-      description: "アルバイト",
-    },
-    // 他のデータも追加
-  ];
+  const [expenses, setExpenses] = useState<FormType[]>([]);
+
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const listsRef = collection(
+          db,
+          "recordlist"
+        ) as CollectionReference<FormType>;
+        const data = await getDocs(
+          query(
+            listsRef,
+            where("uid", "==", auth.currentUser?.uid),
+            orderBy("date", "desc")
+          )
+        );
+        setExpenses(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      } catch (error) {
+        console.error("Error fetching expenses:", error);
+      }
+    };
+
+    fetchExpenses();
+  }, []);
 
   return (
     <Flex
@@ -32,20 +60,22 @@ const RecordListsTable = () => {
         <Table variant="simple">
           <Thead>
             <Tr>
-              <Th>ID</Th>
-              <Th>日付</Th>
-              <Th>タイプ</Th>
+              <Th>購入日時</Th>
               <Th>金額</Th>
+              <Th>収支タイプ</Th>
+              <Th>カテゴリー</Th>
+              <Th>支払い方法</Th>
               <Th>説明</Th>
             </Tr>
           </Thead>
           <Tbody>
             {expenses.map((expense) => (
               <Tr key={expense.id}>
-                <Td>{expense.id}</Td>
-                <Td>{expense.date}</Td>
+                <Td>{expense.date.toDate().toLocaleString()}</Td>
+                <Td>{expense.amount}円</Td>
                 <Td>{expense.type}</Td>
-                <Td>{expense.amount}</Td>
+                <Td>{expense.category}</Td>
+                <Td>{expense.paymentType}</Td>
                 <Td>{expense.description}</Td>
               </Tr>
             ))}

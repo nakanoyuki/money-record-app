1418be486e0753e45f81acadd5dbd3c02c81e1c8
nakanoyuki <yukich41@gmail.com>
2024-05-21 23:42:18 +0900

    🎨: フォームのstyle追加

--- src/components/InputRecordForm/InputRecordFormTable.tsx
+++ src/components/InputRecordForm/InputRecordFormTable.tsx
@@ -47,14 +47,14 @@ const InputRecordFormTable = () => {
     <Flex
       direction="column"
       background="gray.100"
-      padding={8}
-      rounded={6}
+      padding="8"
+      rounded="6"
       backgroundColor="white"
     >
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Flex>
-          <FormControl mb="4">
-            <FormLabel>購入日時</FormLabel>
+        <Flex gap="4">
+          <FormControl>
+            <FormLabel fontWeight="bold">購入日時</FormLabel>
             <Controller
               control={control}
               name="date"
@@ -63,47 +63,49 @@ const InputRecordFormTable = () => {
                   onChange={onChange}
                   selected={value}
                   dateFormat="yyyy/MM/dd"
-                  locale="ja"
                 />
               )}
             />
           </FormControl>
-          <FormControl mb="4">
-            <FormLabel>金額</FormLabel>
-            <Input
-              {...register("amount")}
-              placeholder="金額を入力してください"
-            />
+          <FormControl>
+            <FormLabel fontWeight="bold">金額</FormLabel>
+            <Flex alignItems="center">
+              <Input
+                {...register("amount")}
+                placeholder="金額を入力してください"
+                mr="2"
+              />
+              円
+            </Flex>
           </FormControl>
         </Flex>
-        <Flex>
-          <FormControl mb="4">
-            <FormLabel>収支タイプ</FormLabel>
+        <Flex gap={4}>
+          <FormControl>
+            <FormLabel fontWeight="bold">収支タイプ</FormLabel>
             <Select {...register("type")}>
               <option value="支出">支出</option>
               <option value="収入">収入</option>
             </Select>
           </FormControl>
-          <FormControl mb="4">
-            <FormLabel>カテゴリー</FormLabel>
+          <FormControl>
+            <FormLabel fontWeight="bold">カテゴリー</FormLabel>
             <Select {...register("category")}>
               <option value="交通費">交通費</option>
               <option value="日用品">日用品</option>
             </Select>
           </FormControl>
         </Flex>
-
-        <Flex>
-          <FormControl mb="4">
-            <FormLabel>支払い方法</FormLabel>
+        <Flex gap={4}>
+          <FormControl>
+            <FormLabel fontWeight="bold">支払い方法</FormLabel>
             <Select {...register("paymentType")}>
               <option value="現金">現金</option>
               <option value="クレジット">クレジット</option>
               <option value="QRコード">QRコード</option>
             </Select>
           </FormControl>
-          <FormControl mb="4">
-            <FormLabel>説明</FormLabel>
+          <FormControl>
+            <FormLabel fontWeight="bold">説明</FormLabel>
             <Input
               type="text"
               {...register("description")}
